0f8a813d3018656d8d6180fff907983d1cf2a77a
bishopcanno <ericbispres@gmail.com>
2018-08-19 12:00:39 -0600

    Finished further with cleaner/DRY code

--- assets/css_js_library/crystal_script.js
+++ assets/css_js_library/crystal_script.js
@@ -1,18 +1,13 @@
-// all 4 of the crystal buttons use the same randomization
+// all 4 of the crystal buttons use the same randomization, 1-12
 var redCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
-
-var blueCrystalNumber = Math.floor(Math.random() * 12) + 1;
-
-var purpleCrystalNumber = Math.floor(Math.random() * 12) + 1;
-
-var turquoiseCrystalNumber = Math.floor(Math.random() * 12) + 1;
+var blueCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
+var purpleCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
+var turquoiseCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
 // the target number is 40 through 120
 var randomTargetNumber = Math.floor(Math.random() * 80) + 40;
-
+// various counters used
 var winsCounter = 0;
-
 var lossesCounter = 0;
-
 var userTotalCounter = 0;
 
 
@@ -20,47 +15,31 @@ var userTotalCounter = 0;
 $("#startButton").click( function(){
 // displays the target number for the user    
     $("#randoNumber").html(randomTargetNumber);
-// each of the four crystal buttons invoke the winLoseCondition as well as add themselves to the users total and redisplay that total    
+// each of the four crystal buttons invoke the winLoseCondition as well as add themselves to the users total    
     $("#redCrystalButton").click( function(){
         userTotalCounter = redCrystalNumber + userTotalCounter;
-        $("#totalSpan").html(userTotalCounter);
         winLoseCondition();
     });
     
     $("#blueCrystalButton").click( function(){
         userTotalCounter = blueCrystalNumber + userTotalCounter;
-        $("#totalSpan").html(userTotalCounter);
         winLoseCondition();
     });
     
     $("#purpleCrystalButton").click( function(){
         userTotalCounter = purpleCrystalNumber + userTotalCounter;
-        $("#totalSpan").html(userTotalCounter);
         winLoseCondition();
     });
 
     $("#torquoiseCrystalButton").click( function(){
         userTotalCounter = turquoiseCrystalNumber + userTotalCounter;
-        $("#totalSpan").html(userTotalCounter);
         winLoseCondition();
     });
 });
-// funtion that resets everything except wins losses, used by the winLoseCondition function and reset button
-function resetFunk(){
-    
-    redCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
-    blueCrystalNumber = Math.floor(Math.random() * 12) + 1;
-    purpleCrystalNumber = Math.floor(Math.random() * 12) + 1;
-    turquoiseCrystalNumber = Math.floor(Math.random() * 12) + 1;
-    randomTargetNumber = Math.floor(Math.random() * 80) + 40;
-    userTotalCounter = 0;
-    
-   
-    $("#randoNumber").html(randomTargetNumber);
-    $("#totalSpan").html("0");
-}
-//function to be called each time a crystal button is pushed, will inform if win/loss condition is met
+//function to be called each time a crystal button is pushed, will inform if win/loss condition is met and display the calculation run during an onclick of any crystal button
 function winLoseCondition(){
+    $("#totalSpan").html(userTotalCounter);
+    
     if(userTotalCounter === randomTargetNumber){
         winsCounter++;
         $("#winsSpan").html(winsCounter);
@@ -73,10 +52,30 @@ function winLoseCondition(){
         alert("Better get good at coding because you're a terrible miner!!!");
         resetFunk();
     }
-}
+};
+// funtion that resets everything except wins losses, used by the winLoseCondition function and reset button
+function resetFunk(){
+    
+    redCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
+    blueCrystalNumber = Math.floor(Math.random() * 12) + 1;
+    purpleCrystalNumber = Math.floor(Math.random() * 12) + 1;
+    turquoiseCrystalNumber = Math.floor(Math.random() * 12) + 1;
+    randomTargetNumber = Math.floor(Math.random() * 80) + 40;
+    userTotalCounter = 0;
+       
+    $("#randoNumber").html(randomTargetNumber);
+    $("#totalSpan").html("0");
+};
 // Code for the reset button, calls resetFunk and clears wins/losses
 $("#resetButton").click(function(){
     resetFunk();
     $("#winsSpan").html("0");
     $("#lossesSpan").html("0");
+    lossesCounter = 0;
+    winsCounter = 0;
 });
+
+// Wanted to build a funtion for the randomization of the crystal numbers, not sure exactly how to make that happen.
+// function crystalRandomGen(){
+//     var x = Math.floor(Math.random() * 12 ) + 1;
+// }
