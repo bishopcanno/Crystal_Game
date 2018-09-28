407d98c56fcf5297937d75a0085d20c59833a558
bishopcanno <ericbispres@gmail.com>
2018-09-28 10:50:09 -0600

    Fixed the start button issue with the game by not embedding the crystal buttons within the start button. I used a boolean statement instead and used that as check logic for the crystal and reset buttons. also made it so the start button does nothing once it has been clicked once

--- assets/css_js_library/crystal_script.js
+++ assets/css_js_library/crystal_script.js
@@ -3,39 +3,67 @@ var redCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
 var blueCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
 var purpleCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
 var turquoiseCrystalNumber = Math.floor(Math.random() * 12 ) + 1;
+
 // the target number is 40 through 120
 var randomTargetNumber = Math.floor(Math.random() * 80) + 40;
+
 // various counters used
 var winsCounter = 0;
 var lossesCounter = 0;
 var userTotalCounter = 0;
 
+// determines if the start button has been clicked, built in because the start button being pushed more than once the game would break 
+var startButtonClicked = false;
 
-//upon clicking the start button the game begins and the user can input with the crystal buttons
+//upon clicking the start button the game begins by setting the startButtonClicked var to true
 $("#startButton").click( function(){
-// displays the target number for the user    
+    if(!startButtonClicked)
+    
+    {// displays the target number for the user    
     $("#randoNumber").html(randomTargetNumber);
-// each of the four crystal buttons invoke the winLoseCondition as well as add themselves to the users total    
-    $("#redCrystalButton").click( function(){
+    
+    }
+    startButtonClicked = true;
+});
+
+// each of the four crystal buttons invoke the winLoseCondition as well as add themselves to the users total
+// they also check to make sure the game has been started
+$("#redCrystalButton").click( function(){
+    if(startButtonClicked){
         userTotalCounter = redCrystalNumber + userTotalCounter;
         winLoseCondition();
-    });
-    
-    $("#blueCrystalButton").click( function(){
+    } else {
+        alert("You need to start the game first");
+    }
+});
+
+$("#blueCrystalButton").click( function(){
+    if(startButtonClicked){
         userTotalCounter = blueCrystalNumber + userTotalCounter;
         winLoseCondition();
-    });
-    
-    $("#purpleCrystalButton").click( function(){
+    } else {
+        alert("You need to start the game first");
+    }
+});
+
+$("#purpleCrystalButton").click( function(){
+    if(startButtonClicked){
         userTotalCounter = purpleCrystalNumber + userTotalCounter;
         winLoseCondition();
-    });
+    } else {
+        alert("You need to start the game first");
+    }
+});
 
-    $("#torquoiseCrystalButton").click( function(){
+$("#torquoiseCrystalButton").click( function(){
+    if(startButtonClicked){
         userTotalCounter = turquoiseCrystalNumber + userTotalCounter;
         winLoseCondition();
-    });
+    } else {
+        alert("You need to start the game first");
+    }
 });
+
 //function to be called each time a crystal button is pushed, will inform if win/loss condition is met and display the calculation run during an onclick of any crystal button
 function winLoseCondition(){
     $("#totalSpan").html(userTotalCounter);
@@ -53,6 +81,7 @@ function winLoseCondition(){
         resetFunk();
     }
 };
+
 // funtion that resets everything except wins losses, used by the winLoseCondition function and reset button
 function resetFunk(){
     
@@ -66,16 +95,16 @@ function resetFunk(){
     $("#randoNumber").html(randomTargetNumber);
     $("#totalSpan").html("0");
 };
+
 // Code for the reset button, calls resetFunk and clears wins/losses
 $("#resetButton").click(function(){
-    resetFunk();
-    $("#winsSpan").html("0");
-    $("#lossesSpan").html("0");
-    lossesCounter = 0;
-    winsCounter = 0;
+    if(startButtonClicked){
+        resetFunk();
+        $("#winsSpan").html("0");
+        $("#lossesSpan").html("0");
+        lossesCounter = 0;
+        winsCounter = 0;
+    } else{
+        alert("You need to start the game first")
+    }
 });
-
-// Wanted to build a funtion for the randomization of the crystal numbers, not sure exactly how to make that happen.
-// function crystalRandomGen(){
-//     var x = Math.floor(Math.random() * 12 ) + 1;
-// }
